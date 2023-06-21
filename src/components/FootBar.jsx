b471525d8cfb5d72a504ec7d3f99e2c343a61770
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2023-06-21 16:26:36 -0400

    feat: footer prototype complete

--- src/components/FootBar.jsx
+++ src/components/FootBar.jsx
@@ -14,9 +14,36 @@ function FootBar(){
                     <li><Link to="/Contact">Contact</Link></li>
                 </ul>
             </nav>
-
+            <div className="address">
+                <h1>CRAFT DEVELOPMENT CORPORATION</h1>
+                <p>10 Queen Elizabeth Boulevard, Unit 2</p>
+                <p>Toronto, Ontario</p>
+                <p>M8Z 1L8 Canada</p>
+            </div>
+            <div className="phone">
+                <h1>PHONE</h1>
+                <p>(416)-979-9996</p>
+            </div>
+            <div className="fax">
+                <h1>FAX</h1>
+                <p>(416)-979-0593</p>
+            </div>
+            <div className="tollFree">
+                <h1>TOLL-FREE</h1>
+                <p>1-866-979-9996</p>
+            </div>
+            <div className="email">
+                <h1>EMAIL</h1>
+                <p>INFO@CRAFTDEVELOPMENT.CA</p>
+            </div>
             <img src={tempURL} alt="CRAFT Logo" />
-            <p>Copyright stuff</p>
+            <div className="credit">
+                <p>
+                    Copyright (C) (Year) CRAFT DEVELOPMENT CORPORATION. ALL RIGHTS RESERVED. 
+                    <Link to="/Privacy_Policy">PrivacyPolicy.</Link>
+                </p>
+                <p>WEBSITE DESIGN BY PXPPYHILL ART & DESIGN. DEVELOPMENT BY NICHOLAS PEDRO.</p>
+            </div>
         </div>
     )
 }
