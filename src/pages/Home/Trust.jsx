8c5c624f8b074f3ee7f5cc5de1b56330d41a461e
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2023-06-28 11:17:32 -0400

    feat: completed company trust list

---
+++ src/pages/Home/Trust.jsx
@@ -0,0 +1,73 @@
+import "../../assets/trust.scss"
+
+function Trust() {
+  return (
+    <div className="trust">
+      <h1>COMPANIES WHO TRUST CRAFT</h1>
+      <ul className="trust__companies">
+        <li className="trust__item">
+          <p className="trust__company">Bank of Montreal</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Chapters</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Dollarama</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">East Side Mario's</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Marks</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Mr.Sub</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Petro Canada</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Pizza Pizza</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">RBC</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Rexall</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Shoppers Drug Mart</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Staples</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">St. Louis Bar & Grill</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Subway</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">TD Canada Trust</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Tim Horton's</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Value Village</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Walmart</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">Zehr's</p>
+        </li>
+        <li className="trust__item">
+          <p className="trust__company">And More!</p>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Trust
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,16 @@
 import SearchBar from "../../components/SearchBar"
+import Trust from "./Trust"
 import Questions from "./Questions"
 
-function Home(){
-    return <>
-        <h1>Home</h1>
-        <SearchBar/>
-        <Questions/>
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <SearchBar />
+      <Trust />
+      <Questions />
     </>
+  )
 }
 
 export default Home
