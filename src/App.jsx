c78d165efe82e8e6a712fbce6df400afd25eaa2e
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2023-07-07 22:46:47 -0400

    feat: completed home page

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,17 @@ import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-    <>
-      <NavBar/>
+    <div style={{ overflowX: "hidden" }}>
+      <NavBar />
       <Routes>
-        <Route path='/' element={Home()}/>
-        <Route path='/About' element={About()}/>
-        <Route path='/Services' element={Services()}/>
-        <Route path='/Properties' element={Properties()}/>
-        <Route path='/Contact' element={Contact()}/>
+        <Route path="/" element={Home()} />
+        <Route path="/About" element={About()} />
+        <Route path="/Services" element={Services()} />
+        <Route path="/Properties" element={Properties()} />
+        <Route path="/Contact" element={Contact()} />
       </Routes>
-      <Footer/>
-    </>
+      <Footer />
+    </div>
   )
 }
 
