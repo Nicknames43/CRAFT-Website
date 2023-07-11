7775f6e4117e92d974aa2873c81ab9fd19a4649e
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2023-07-11 15:45:27 -0400

    feat: image carousel modularized

--- src/pages/Home/FeatureProperties.jsx
+++ src/pages/Home/FeatureProperties.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect, useState, useRef, useCallback } from "react"
+import PropTypes from "prop-types"
 import keswickLogo from "/images/keswick-marketplace-logo.svg"
 import tempLogo from "/images/temp-img.svg"
-import arrow from "/images/next slide arrow.svg"
+import Carousel from "../../components/Carousel"
 import "../../assets/featureProperties.scss"
 
-function FeatureProperty({ item }) {
+function FeatureProperty({ icon, title, description, infoLink }) {
   return (
     <div className="carousel-item">
-      <img src={item.icon} alt={item.title} className="carousel-item__img" />
+      <img src={icon} alt={title} className="carousel-item__img" />
       <div className="carousel-item__text">
-        <p className="carousel-item__description">{item.description}</p>
+        <p className="carousel-item__description">{description}</p>
         <br />
         <p className="carousel-item__info-link">
           For more information, please visit:{" "}
-          <a href={`https://www.${item.infoLink}`}>{item.infoLink}</a>
+          <a href={`https://www.${infoLink}`}>{infoLink}</a>
         </p>
       </div>
     </div>
   )
 }
 
+FeatureProperty.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  infoLink: PropTypes.string.isRequired,
+}
+
 function FeatureProperties() {
-  const innerRef = useRef(null)
-  const timerRef = useRef(null)
-  const [currIndex, setCurrIndex] = useState(1)
-  const [translateX, setTranslateX] = useState(0)
-  const [autoPlay, setAutoPlay] = useState(true)
   const items = [
     {
       title: "ORCA Project",
@@ -51,68 +53,17 @@ function FeatureProperties() {
     },
   ]
   const slideSpeed = "0.5s"
-
-  const goToNextSlide = useCallback(() => {
-    innerRef.current.style.transitionDuration = slideSpeed
-    if (currIndex >= items.length) {
-      setTranslateX(100 * (items.length + 1))
-      setCurrIndex(1)
-    } else {
-      setTranslateX(100 * (currIndex + 1))
-      setCurrIndex(currIndex + 1)
-    }
-  }, [currIndex, items])
-
-  const goToPrevSlide = () => {
-    innerRef.current.style.transitionDuration = slideSpeed
-    if (currIndex <= 1) {
-      setTranslateX(0)
-      setCurrIndex(items.length)
-    } else {
-      setTranslateX(100 * (currIndex - 1))
-      setCurrIndex(currIndex - 1)
-    }
+  const slides = []
+  for (let i = 0; i < items.length; i++) {
+    slides[i] = (
+      <FeatureProperty
+        icon={items[i].icon}
+        title={items[i].title}
+        description={items[i].description}
+        infoLink={items[i].infoLink}
+      />
+    )
   }
-
-  const goToSlide = (newIndex) => {
-    innerRef.current.style.transitionDuration = slideSpeed
-    setCurrIndex(newIndex)
-    setTranslateX(100 * newIndex)
-  }
-
-  useEffect(() => {
-    const transitionEnd = () => {
-      if (currIndex <= 1) {
-        innerRef.current.style.transitionDuration = "0ms"
-        setTranslateX(100 * currIndex)
-      }
-
-      if (currIndex >= items.length) {
-        innerRef.current.style.transitionDuration = "0ms"
-        setTranslateX(100 * items.length)
-      }
-    }
-
-    document.addEventListener("transitionend", transitionEnd)
-
-    return () => {
-      document.removeEventListener("transitionend", transitionEnd)
-    }
-  }, [currIndex, items])
-
-  useEffect(() => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current)
-    }
-    timerRef.current = setTimeout(() => {
-      goToNextSlide()
-    }, 5000)
-    if (!autoPlay) {
-      clearTimeout(timerRef.current)
-    }
-
-    return () => clearTimeout(timerRef.current)
-  }, [goToNextSlide])
 
   return (
     <div className="feature-properties">
@@ -147,72 +98,12 @@ function FeatureProperties() {
           </div>
         </foreignObject>
         <foreignObject x="100" y="157" width="1702" height="450">
-          <div
-            xmlns="http://www.w3.org/1999/xhtml"
-            className="feature-properties__carousel carousel"
-            onMouseEnter={() => {
-              setAutoPlay(false)
-            }}
-            onMouseLeave={() => {
-              setAutoPlay(true)
-            }}
-          >
-            <div
-              className="carousel__inner"
-              ref={innerRef}
-              style={{ transform: `translate(-${translateX}%)` }}
-            >
-              <FeatureProperty key="fp0" item={items[items.length - 1]} />
-              {items.map((item) => {
-                return <FeatureProperty key={item.id} item={item} />
-              })}
-              <FeatureProperty key={`fp${items.length + 1}`} item={items[0]} />
-            </div>
-            <div className="carousel__buttons">
-              <button onClick={goToPrevSlide} className="carousel__left">
-                <img src={arrow} alt="Prev" className="carousel__left-arrow" />
-              </button>
-              <div className="carousel__indicators">
-                {items.map((item, index) => {
-                  return (
-                    <button
-                      key={`${item.title} button`}
-                      className="carousel__indicator"
-                      onClick={() => {
-                        goToSlide(index + 1)
-                      }}
-                    >
-                      <svg viewBox="0 0 90.792 24.827">
-                        <g transform="translate(-798.035 -3283.99)">
-                          <path
-                            d="M403.934-174.07l-24.574,14.827h30.287l24.574-14.827Z"
-                            transform="translate(436.639 3463.06)"
-                            fill="#fff"
-                            style={{
-                              transform: `opacity ${slideSpeed}`,
-                              opacity: `${index + 1 === currIndex ? 0.399 : 1}`,
-                            }}
-                          />
-                          <path
-                            d="M403.934-174.07l-24.574,14.827h30.287l24.574-14.827H403.934m-1.392-5h49.644L411.04-154.243H361.4Z"
-                            transform="translate(436.639 3463.06)"
-                            fill="#fff"
-                            style={{
-                              transform: `opacity ${slideSpeed}`,
-                              opacity: `${index + 1 === currIndex ? 1 : 0}`,
-                            }}
-                          />
-                        </g>
-                      </svg>
-                    </button>
-                  )
-                })}
-              </div>
-              <button onClick={goToNextSlide} className="carousel__right">
-                <img src={arrow} alt="Next" className="carousel__right-arrow" />
-              </button>
-            </div>
-          </div>
+          <Carousel
+            className="feature-properties__carousel"
+            items={slides}
+            itemPrefix="fp"
+            slideSpeed={slideSpeed}
+          ></Carousel>
         </foreignObject>
       </svg>
     </div>
