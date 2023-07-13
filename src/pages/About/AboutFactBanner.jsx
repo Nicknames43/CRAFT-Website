d83813115bf1e2fa65ffda78fe6a3ba55d4b2290
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2023-07-13 10:06:11 -0400

    feat: about page completed

---
+++ src/pages/About/AboutFactBanner.jsx
@@ -0,0 +1,49 @@
+import "../../assets/aboutFactBanner.scss"
+
+function AboutFactBanner() {
+  return (
+    <div className="fact-banner">
+      <div className="fact-banner__bg">
+        <svg viewBox="0 0 2464.298 122.465">
+          <g id="rectangles" transform="translate(291.934 -2466.768)">
+            <path
+              id="search_section"
+              data-name="search section"
+              d="M-27.847,7986.658H1841.118l-160.449,100H-188.3Z"
+              transform="translate(1949.118 10564.658) rotate(180)"
+              fill="#233682"
+            />
+            <path
+              id="search_section_-_Outline"
+              data-name="search section - Outline"
+              d="M-27.847,7986.658l-160.448,100H1680.67l160.448-100H-27.847m-2.861-10H1876.069l-192.538,120H-223.246Z"
+              transform="translate(1949.118 10564.658) rotate(180)"
+              fill="#233682"
+            />
+            <path
+              id="Path_248"
+              data-name="Path 248"
+              d="M77.8,152.823H393.467L594.739,30.358H279.077"
+              transform="translate(-369.739 2436.411)"
+              fill="#2869a8"
+            />
+          </g>
+          <foreignObject x="561" y="12" width="1400" height="99">
+            <div
+              className="fact-banner__container"
+              xmlns="http://www.w3.org/1999/xhtml"
+            >
+              <p className="fact-banner__fact">
+                SINCE ITS INCEPTION, CRAFT HAS BEEN INVOLVED IN THE DEVELOPMENT
+                OF 1.8 MILLION + SQUARE FEET OF RETAIL, OFFICE, & RESIDENTIAL
+                PROJECTS.
+              </p>
+            </div>
+          </foreignObject>
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+export default AboutFactBanner
